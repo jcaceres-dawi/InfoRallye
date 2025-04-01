2d1445287e7cf6ec32a350d0d5ffc8bcfe5f20f0
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-04-01 10:51:52 +0200

    Update date format

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>{{ $category->name }}</h1>
-        <p>{{ $category->description }}</p>
+<div class="container">
+    <h1>{{ $category->name }}</h1>
+    <p>{{ $category->description }}</p>
 
-        <h3>Rallys de esta categoría</h3>
-        <div class="list-group">
-            @forelse($rallies as $rally)
-                <a href="{{ $rally->website }}" target="_blank" class="list-group-item list-group-item-action">
-                    {{ $rally->name }} - {{ $rally->location }} ({{ $rally->start_date }} - {{ $rally->end_date }})
-                </a>
-            @empty
-                <p>No hay rallies programados para esta categoría.</p>
-            @endforelse
-        </div>
+    <h3>Rallys de esta categoría</h3>
+    <div class="list-group">
+        @forelse($rallies as $rally)
+        <a href="{{ $rally->website }}" target="_blank" class="list-group-item list-group-item-action">
+            {{ $rally->name }} - {{ $rally->location }} ({{ \Carbon\Carbon::parse($rally->start_date)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($rally->end_date)->format('d/m/Y') }})
+        </a>
+        @empty
+        <p>No hay rallies programados para esta categoría.</p>
+        @endforelse
     </div>
+</div>
 @endsection
