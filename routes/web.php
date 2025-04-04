acde5fbfbbc3084364f492efd4e6399eae45de2f
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-04-04 17:08:14 +0200

    More endpoints

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DriverController;
+use App\Http\Controllers\RallyController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,5 +12,9 @@ Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/competitors', [DriverController::class, 'index'])->name('competitors.index');
 
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+
 Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
+
 Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
+
+Route::get('/rallys/{rally}', [RallyController::class, 'index'])->name('rallys.index');
