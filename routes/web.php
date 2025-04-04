50d8030fb699f14bc096ce6f09ca5ff644bee30b
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-04-04 16:08:06 +0200

    Add urls

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,13 @@
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DriverController;
+use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
 
 Route::get('/competitors', [DriverController::class, 'index'])->name('competitors.index');
 
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
+Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
