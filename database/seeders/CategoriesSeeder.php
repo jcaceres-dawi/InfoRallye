3afd3ca7d1cbfd3fce18894afb491a1d00026b5d
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2025-05-14 20:52:39 +0200

    Update data

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -13,10 +13,10 @@ class CategoriesSeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'S-CER', 'description' => 'Supercampeonato de España de Rallyes.'],
-            ['name' => 'CERA Recalvi', 'description' => 'Campeonato de España de Rallyes de Asfalto.'],
-            ['name' => 'CERT Rallycar', 'description' => 'Campeonato de España de Rallyes de Tierra.'],
-            ['name' => 'CERTT GT2i', 'description' => 'Campeonato de España de Rallyes Todo Terreno.'],
+            ['name' => 'S-CER', 'description' => 'Supercampeonato de España de Rallyes'],
+            ['name' => 'CERA Recalvi', 'description' => 'Campeonato de España de Rallyes de Asfalto'],
+            ['name' => 'CERT Rallycar', 'description' => 'Campeonato de España de Rallyes de Tierra'],
+            ['name' => 'CERTT GT2i', 'description' => 'Campeonato de España de Rallyes Todo Terreno'],
         ];
 
         foreach ($categories as $category) {
